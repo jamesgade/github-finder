9c57cca13c1c64f9c446bc24c250d350c910eebd
James Gade <57884768+jamesgade@users.noreply.github.com>
2023-06-16 11:26:21 +0530

    Update Socials.jsx

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -6,7 +6,7 @@ function Socials() {
             <a target="_blank" rel="noreferrer" href='https://twitter.com/authorgreenleaf' className='m-2'>
                 <FaTwitter size={30} />
             </a>
-            <a target="_blank" rel="noreferrer" href='https://github.com/jamesleaf' className='m-2'>
+            <a target="_blank" rel="noreferrer" href='https://github.com/jamesgade' className='m-2'>
                 <FaGithub size={30} />
             </a>
         </div>
